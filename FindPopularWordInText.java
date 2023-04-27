a1d40b7d2c31f6566300cdb4f30a4d6f525aeb99
Nikolai Orlov <semberk1297@gmail.com>
2023-04-27 17:08:00 +0300

    now the program can remove extra characters and return the correct text

--- FindPopularWordInText.java
+++ FindPopularWordInText.java
@@ -1,33 +1,50 @@
 public class FindPopularWordInText {
     public static void main(String[] args) {
-        String text = "hello java world java hello I Hello Java World java";
-        System.out.println(findPopularWord(text));
+        String text = "hello,java-world\tjava hello: I? Hello! Java World. java hello hello";
+
+        String newText = getWordsFromText(text);
+        System.out.println(findPopularWord(newText));
     }
 
-        private static String findPopularWord(String text) {
-            String[] strArray = text.split(" ");
 
-            int[] wordRepeats = new int[strArray.length];
-            int wordRepeatsCount = 0;
-            for (int i = 0; i < strArray.length; i++) {
-                String currentWord = strArray[i];
-                for (int j = 0; j < strArray.length; j++) {
-                    if (currentWord.equalsIgnoreCase(strArray[j])){
-                        wordRepeatsCount++;
-                    }
-                }
-                wordRepeats[i] = wordRepeatsCount;
-                wordRepeatsCount = 0;
+    private static String getWordsFromText(String text) {
+        StringBuilder newText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isAlphabetic(text.charAt(i))) {
+                newText.append(text.charAt(i));
+            } else {
+                newText.append(" ");
             }
-
-            int indexOfTheMostPopularWord = 0;
-            for (int i = 0; i < wordRepeats.length; i++) {
-                if (indexOfTheMostPopularWord < wordRepeats[i]){
-                    indexOfTheMostPopularWord = i;
-                }
-            }
-
-            return strArray[indexOfTheMostPopularWord];
         }
+
+        return newText.toString();
     }
+
+    private static String findPopularWord(String newText) {
+        String[] strArray = newText.split("\\s+");
+
+        int[] wordRepeats = new int[strArray.length];
+        int wordRepeatsCount = 0;
+        for (int i = 0; i < strArray.length; i++) {
+            String currentWord = strArray[i];
+            for (int j = 0; j < strArray.length; j++) {
+                if (currentWord.equalsIgnoreCase(strArray[j])) {
+                    wordRepeatsCount++;
+                }
+            }
+            wordRepeats[i] = wordRepeatsCount;
+            wordRepeatsCount = 0;
+        }
+
+        int indexOfTheMostPopularWord = 0;
+        for (int i = 0; i < wordRepeats.length; i++) {
+            if (indexOfTheMostPopularWord < wordRepeats[i]) {
+                indexOfTheMostPopularWord = i;
+            }
+        }
+
+        return strArray[indexOfTheMostPopularWord];
+    }
+}
 
